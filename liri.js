96fe10fc2b12169820422cce9c5282c69f644112
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-10-03 00:35:38 -0600

    xxxxxxxx

--- liri.js
+++ liri.js
@@ -29,6 +29,12 @@ function getTweets () {
 						console.log('Tweet Number: ' + (i+1));
 	 					console.log('@SummitMedia1 tweeted on: ' + data[i].created_at);
 	 					console.log('Message:' + data[i].text);
+	 					fs.appendFile('log.txt', "=================================================================");
+			 			fs.appendFile('log.txt', "Here are @SummitMedia1's latest tweets:");
+						fs.appendFile('log.txt', "_______________________________________________________");
+						fs.appendFile('log.txt', 'Tweet Number: ' + (i+1));
+	 					fs.appendFile('log.txt', '@SummitMedia1 tweeted on: ' + data[i].created_at);
+	 					fs.appendFile('log.txt', 'Message:' + data[i].text);
 	 					}
 
 }
@@ -62,7 +68,13 @@ function getSong(Song) {
 								console.log('Song Name: ' + music.name + '.\r\n');
 								console.log('Album Name: ' + music.album.name + '.\r\n');
 								console.log('Preview Song Url: ' + music.preview_url + '.\r\n');
-								console.log("-----------------------------------------------------------------" + '\r\n');
+								fs.appendFile('log.txt', '-----------------------------------------------------------------' + '\r\n');
+								fs.appendFile('log.txt', '-----------------------------------------------------------------' + '\r\n');
+								fs.appendFile('log.txt', 'Artist Name(s): ' + music.artists[0].name + '.\r\n');
+								fs.appendFile('log.txt', 'Song Name: ' + music.name + '.\r\n');
+								fs.appendFile('log.txt', 'Album Name: ' + music.album.name + '.\r\n');
+								fs.appendFile('log.txt', 'Preview Song Url: ' + music.preview_url + '.\r\n');
+								fs.appendFile('log.txt', "-----------------------------------------------------------------" + '\r\n');
 						}
 });
 }
@@ -106,6 +118,25 @@ function getMovie() {
                console.log('---------------------------------------------------------');
                console.log("Rotten Tomatoes URL: " + movieData.tomatoURL);
                console.log('---------------------------------------------------------');
+            fs.appendFile('log.txt', '------------------------------------------------------------------');
+			fs.appendFile('log.txt', 'Title: ' + movieData.Title + '.\r\n');	
+			fs.appendFile(" ");					
+			fs.appendFile('log.txt', "Release Year: " + movieData.Year + '.\r\n');
+			fs.appendFile(" ");				
+			fs.appendFile('log.txt', "IMdB Rating: " + movieData.imdbRating + '.\r\n');
+			fs.appendFile(" ");			
+			fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + movieData.tomatoRating + '.\r\n');
+			fs.appendFile(" ");
+			fs.appendFile('log.txt', "Rotten Tomatoes URL: " + movieData.tomatoURL + '.\r\n');
+			fs.appendFile(" ");	
+			fs.appendFile('log.txt', "Country: " + movieData.Country + '.\r\n');
+			fs.appendFile(" ");	
+			fs.appendFile('log.txt', "Language: " + movieData.Language + '.\r\n');
+			fs.appendFile(" ");	
+			fs.appendFile('log.txt', "Plot: " + movieData.Plot + '.\r\n');
+			fs.appendFile(" ");	
+			fs.appendFile('log.txt', "Actors: " + movieData.Actors + '.\r\n');					
+			fs.appendFile('log.txt', "------------------------------------------------------------------");
           } else {
                console.log(err);
           }
@@ -154,4 +185,5 @@ default:
 break;
 }
 //appends the arugments to the log.txt file
-fs.appendFile("log.txt", process.argv + "\n");
+fs.appendFile('log.txt', process.argv + "\n");
+
